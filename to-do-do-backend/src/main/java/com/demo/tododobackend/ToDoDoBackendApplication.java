127cf1016b42fd78bb486151f171c949469861f3
Volotskov <volockove@gmail.com>
2022-04-17 20:54:07 +0300

    initial commit

---
+++ to-do-do-backend/src/main/java/com/demo/tododobackend/ToDoDoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo.tododobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoDoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoDoBackendApplication.class, args);
+	}
+
+}
