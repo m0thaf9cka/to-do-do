b994741f8db1133ba3c6298fad0b0a05fbf18a5d
Volotskov <volockove@gmail.com>
2022-05-08 12:37:17 +0300

    start migration to typescript

--- to-do-do-frontend/src/components/TodoButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/components/TodoButton.scss";
-
-const TodoButton = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
-};
-
-export default TodoButton;
--- to-do-do-frontend/src/components/TodoInput.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/components/TodoInput.scss";
-
-const TodoInput = React.forwardRef((props, ref) => {
-  return <input className={"todoInput"} {...props} ref={ref} />;
-});
-
-export default TodoInput;
